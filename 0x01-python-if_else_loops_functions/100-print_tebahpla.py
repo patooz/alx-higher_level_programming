5d338c020c67d4bca731e451bf69a89242598626
Patrick Kipsum <pat.koech01@gmail.com>
2023-08-08 12:54:01 -0700

    fixed an error

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 x = 0
 for y in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(char(y - x)), end="")
+    print("{}".format(chr(y - x)), end="")
     x = 32 if x == 0 else 0
