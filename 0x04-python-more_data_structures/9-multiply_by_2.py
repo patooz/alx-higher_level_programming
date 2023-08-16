a5482986462c0271b6b9ae6e78f3435489b272bf
Patrick Kipsum <pat.koech01@gmail.com>
2023-08-16 09:36:13 -0700

    fixed an issue

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     brand_item = {}
     for key, value in a_dictionary.items():
-        brand_item.update({ke: (value * 2)})
+        brand_item.update({key: (value * 2)})
     return brand_item
