a2b5b6a265e9b769053a3bc134e06802c62f82a1
Patrick Kipsum <pat.koech01@gmail.com>
2023-08-29 05:57:52 -0700

    fixed an error

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,3 +1,12 @@
+#!/usr/bin/python3
+
+""" class Square definition """
+
+
 class Square:
+    """square representaion """
+
     def __init__(self, size):
+        """ initialization if the new square """
+
         self.__size = size
