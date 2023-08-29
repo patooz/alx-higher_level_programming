d3b39a0cbd7eb086b3c4a712ade8614dc7f3f658
Patrick Kipsum <pat.koech01@gmail.com>
2023-08-29 06:05:09 -0700

    fixed an issue

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,14 @@
+#!/usr/bin/python3
+
+""" class square definition """
+
+
 class Square:
+    """square representation """
+    
     def __init__(self, size=0):
+        """ initialization if the new square """
+
         if not isinstance(size, int):
             raise TypError("size must be an integer")
         elif size < 0:
