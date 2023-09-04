c82513861e86626fad8f4794134967b9de2b1657
Patrick Kipsum <pat.koech01@gmail.com>
2023-09-04 23:04:52 +0300

    fixed an error

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     """ a class that reperesents a rectangle """
     number_of_instances = 0
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
         """ new rectangle initialization """
