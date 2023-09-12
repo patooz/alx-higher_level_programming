8048d3f4e272ea05f3b01b828accfae5585f1cf1
Patrick Kipsum <pat.koech01@gmail.com>
2023-09-12 18:03:35 +0300

    10. Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -11,7 +11,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """ dictionary representation of Student """
         if (type(attrs) == list
                 and all(type(x) == str for x in attrs)):
