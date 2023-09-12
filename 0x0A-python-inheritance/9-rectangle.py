5a3e2e436d829c2abf27060e16e699d0f9008b4d
Patrick Kipsum <pat.koech01@gmail.com>
2023-09-12 11:11:37 +0300

    fixed an error

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -17,8 +17,8 @@ class Rectangle(BaseGeometry):
         """ area of the rectangle """
         return self.__width * self.__height
 
-    def __str__(self)
-    """ print and str of representation of the rectangle """
-    rep = "[" + str(self.__class__.__name__) + "] "
-    rep += str(self.__width) + "/" + str(self.__height)
-    return rep
+    def __str__(self):
+        """ print and str of representation of the rectangle """
+        rep = "[" + str(self.__class__.__name__) + "] "
+        rep += str(self.__width) + "/" + str(self.__height)
+        return rep
