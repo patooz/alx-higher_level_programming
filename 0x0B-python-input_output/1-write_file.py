e83aabdeb9958c4085fb96a8081182d6d32550a2
Patrick Kipsum <pat.koech01@gmail.com>
2023-09-12 16:13:52 +0300

    2. Append to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" defines a function that writes into a file """
+
+
+def append_write(filename="", text=""):
+    """ writes to a utf8 encodeing """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
--- 0x0B-python-input_output/README.md
+++ 0x0B-python-input_output/README.md
@@ -1,1 +1,2 @@
 0-read_file.py - a function that reads a text file (UTF8) and prints it to stdout:
+1-write_file.py a function that appends a string at the end of a text file (UTF8) and returns the number of characters added
