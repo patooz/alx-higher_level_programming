09f3ff212cab550a26fbd77d763f5ff4a051cb33
Patrick Kipsum <pat.koech01@gmail.com>
2023-09-12 16:30:37 +0300

    fixed an issue

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,7 +2,7 @@
 """ defines a function that writes into a file """
 
 
-def append_write(filename="", text=""):
+def write_file(filename="", text=""):
     """ writes to a utf8 encodeing """
     with open(filename, "w", encoding="utf-8") as y:
         return y.write(text)
